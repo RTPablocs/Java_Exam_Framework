b6196961bf03863b661e2d3f88022f02f0022a3c
RTPablocs <conejerosorianopablo@gmail.com>
2020-10-09 12:48:41 +0200

    Added extract_digits, multiply, and sun functions

--- src/exam_functions.java
+++ src/exam_functions.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class exam_functions {
     static int calculate_median(ArrayList<Integer> median_number) {
@@ -19,8 +20,52 @@ public class exam_functions {
             }
 
         }
-        return(dividends);
+        return (dividends);
     }
 
 
+    static ArrayList<Integer> extract_digits(int num) {
+        ArrayList<Integer> digits = new ArrayList<>();
+        while (num > 0) {
+            digits.add(num % 10);
+            num = num / 10;
+        }
+        Collections.sort(digits);
+        return (digits);
+    }
+
+    static int multiply_from_even_digits(ArrayList<Integer> digits) {
+        int product = 1; //Initialized to 1 because the product, and isn't possible to swap variable value
+        for (Integer digit : digits) {
+            if ((digit % 2) == 0) {
+                product *= digit;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Your result is: " + product);
+        return product;
+    }
+
+    static int multiply_from_odd_digits(ArrayList<Integer> digits) {
+        int product = 1; //Initialized to 1 because the product, and isn't possible to swap variable value
+        for (Integer digit : digits) {
+            if ((digit % 2) != 0) {
+                product *= digit;
+            }
+        }
+        return product;
+    }
+
+    static int sum_before_num(int end_num) {
+        boolean kill_switch = false;
+        int result = 0, num = 0;
+        while (!kill_switch) {
+            num = Integer.parseInt(JOptionPane.showInputDialog(null, "Insert the number"));
+            if (num == end_num) {
+                kill_switch = true;
+            }
+            result += num;
+        }
+        JOptionPane.showMessageDialog(null, "Result is: " + result);
+        return result;
+    }
 }
